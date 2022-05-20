8779cb4f10f6ff72a7ced4be15abc0068f02e133
Tyler Goodwin Jr <105366372+tmangoodie@users.noreply.github.com>
2022-05-20 08:45:26 -0400

    Add files via upload

---
+++ Original_Code.py
@@ -0,0 +1,13 @@
+#Tyler Goodwin
+#CNS1, Original Python Code
+#The code will be a menu that allows me to open applications I use everyday
+#The code is being built so I can get around my home computer easier.
+#The code will be able to open and close applications, possibly input an application name and it will open/close it
+#-----------------------------------------------------------------------------------
+import os
+import subprocess
+print (os.system('notepad.exe'))
+print("Hello, welcome to the PC!")
+print("I'm still a work in progress so have patience.")
+print("My creator has big plans for me.")
+print("What would you like to do?")
